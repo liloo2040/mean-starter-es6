c1d824fdab002ef8fb8f9276069de38dd1093662
liloo2040 <e.dufour.89@gmail.com>
2016-07-22 18:07:54 +0200

    ajout routes et contact

---
+++ server/api/routes/contacts.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import Contact from '../models/contact.js';
+
+let router = express.Router();
+
+module.exports = (app) => {
+
+    var contact = new Contact();
+
+    router.get('/', contact.findAll);
+
+    router.get('/:id', contact.findById);
+
+    router.post('/', contact.create);
+
+    router.put('/:id', contact.update);
+
+    router.delete('/:id', contact.delete);
+
+    app.use('/contacts', router);
+
+}
